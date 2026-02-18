852d85957b3cce1471002f988b3800fd395f8cc7
shubham874p <140966043+shubham874p@users.noreply.github.com>
2026-02-18 15:27:27 +0530

    Add JwtConfig class for API key configuration

---
+++ config/JwtConfig.java
@@ -0,0 +1,18 @@
+package edu.usc.csci310.project.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtConfig{
+
+    @Value("${api.key}")
+    private String apiKey;
+
+//    VWaLngE0ePnBA7PZECEEzk3QritRKGaHmRTWnXCK
+
+    // Getter
+    public String getApiKey() {
+        return apiKey;
+    }
+}
